caefeb9c52b7fb6128f75c7346d28d564f8698f6
윤유섭 <166105837+uson1004@users.noreply.github.com>
2024-05-15 20:25:34 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- C99/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
+++ C99/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int N = 0;
-    scanf("%d", &N);
-    
-    for (int i = 1; i <= N; i++) {
+    int n = 0;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
         for (int j = 0; j < i; j++) {
             printf("*");
         }
         printf("\n");
     }
-     
 }
