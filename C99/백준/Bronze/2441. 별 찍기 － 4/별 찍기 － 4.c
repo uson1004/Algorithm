a24c05c948dc1e90a6a76443dc6a8737801ce143
윤유섭 <166105837+uson1004@users.noreply.github.com>
2024-06-07 17:30:08 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    
+    scanf("%d", &n);
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            printf(" ");
+        }
+        for(int j = 0; j < n - i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
