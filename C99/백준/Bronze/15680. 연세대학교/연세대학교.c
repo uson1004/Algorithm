de743bd5b45f4338d947a0d0721b757a860d7bbf
윤유섭 <166105837+uson1004@users.noreply.github.com>
2024-06-18 11:51:33 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    int num = 0;
+    
+    scanf("%d", &num);
+    
+    if (num != 1)
+    printf("YONSEI");
+    else
+    printf("Leading the Way to the Future");
+}
