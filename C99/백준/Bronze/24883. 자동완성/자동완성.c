cb8a3b0f06db77e390d0c9a8fbbc547db6c9fb8e
윤유섭 <166105837+uson1004@users.noreply.github.com>
2024-06-18 11:52:02 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/24883. 자동완성/자동완성.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+        
+    char D2;
+    
+    scanf("%c", &D2);
+    
+    if (D2 == 'N' || D2 == 'n')
+    printf("Naver D2");
+    else
+    printf("Naver Whale");
+    
+}
